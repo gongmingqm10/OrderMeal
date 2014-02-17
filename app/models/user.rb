5f6c558bd359263de0790ec38dcd69fe2071c091
gongming <gongmingqm10@foxmail.com>
2014-02-17 23:33:43 +0800

    [gongming] #401 BCrypt user password to secret hash in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,35 @@ class User
   include Mongoid::Timestamps
 
   field :username, type: String
-  field :password, type: String
+  field :password_hash, type: String
+  field :password_salt, type: String
   field :role, type: String
   field :email, type: String
   field :phone, type: String
+
+  attr_accessor :password
+
   validates_presence_of( :username, :password)
 
   embeds_one :shop
   has_many :foods
+  before_save :encrypt_password
 
   class << self
     def authenticate (username, password)
-      user = find_by(username: username, password: password)
+      user = find_by(username: username)
+      if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+        user
+      end
+    end
+  end
+
+  private
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
     end
   end
 
